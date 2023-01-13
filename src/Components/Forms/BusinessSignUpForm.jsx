1bce2018077db004ab9b6f5377ea411583df8687
mcthuggets <dreu0001@student.monash.edu>
2023-01-13 13:28:21 +0200

    take user input from form fields

--- src/Components/Forms/BusinessSignUpForm.jsx
+++ src/Components/Forms/BusinessSignUpForm.jsx
@@ -1,6 +1,30 @@
 import React from 'react'
 
 export default function BusinessSignUpForm() {
+
+    let firstName = ''
+    let lastName = ''
+    let businessName = ''
+    let email = ''
+    let contactNumber = ''
+
+    let password = ''
+    let confirmedPassword = ''
+
+    let marketingConsent = ''
+
+
+    function handleClick(event) {
+        console.log(firstName)
+        console.log(lastName)
+        console.log(businessName)
+        console.log(email)
+        console.log(contactNumber)
+        console.log(password)
+        console.log(confirmedPassword)
+        console.log(marketingConsent)
+    }
+
   return (
     <>
         <div className=' p-36 m-1'>
@@ -19,7 +43,8 @@ export default function BusinessSignUpForm() {
                 </div>
                 
                 <div>
-                    <input type="text" placeholder='Placeholder' className=' placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal rounded border-2 border-gray-500 w-full p-3 mb-2'></input>
+                    <input type="text" placeholder='f name' className=' placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal rounded border-2 border-gray-500 w-full p-3 mb-2' 
+                            onChange={ (event) => { firstName =  event.target.value } }></input>
                 </div>
                 
                 <div>
@@ -27,7 +52,8 @@ export default function BusinessSignUpForm() {
                 </div>
 
                 <div>
-                    <input type="text" placeholder='Placeholder' className=' placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'></input>
+                    <input type="text" placeholder='Placeholder' className=' placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'
+                            onChange={ (event) => { lastName =  event.target.value } }></input>
                 </div>
             </div>
 
@@ -37,7 +63,8 @@ export default function BusinessSignUpForm() {
                 </div>
 
                 <div>
-                    <input type="text" placeholder='Placeholder' className=' placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'></input>
+                    <input type="text" placeholder='Placeholder' className=' placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'
+                            onChange={ (event) => { businessName =  event.target.value } }></input>
                 </div>
 
             {/** EMAIL + CONTACT */}
@@ -47,7 +74,8 @@ export default function BusinessSignUpForm() {
                 </div>
 
                 <div>
-                    <input type="email" placeholder='Email' className='placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'></input>
+                    <input type="email" placeholder='Email' className='placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'
+                            onChange={ (event) => { email =  event.target.value } }></input>
                 </div>
 
                 <div>
@@ -55,7 +83,8 @@ export default function BusinessSignUpForm() {
                 </div>
 
                 <div>
-                    <input type="tel" placeholder='Placeholder' className='placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'></input>
+                    <input type="tel" placeholder='Placeholder' className='placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'
+                            onChange={ (event) => { contactNumber =  event.target.value } }></input>
                 </div>
 
             </div>
@@ -67,7 +96,8 @@ export default function BusinessSignUpForm() {
                     </div>
 
                     <div>
-                        <input type="password" placeholder='Placeholder' className='placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'></input>
+                        <input type="password" placeholder='Placeholder' className='placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'
+                                onChange={ (event) => { password =  event.target.value } }></input>
                     </div>
                     
                     <div>
@@ -75,18 +105,21 @@ export default function BusinessSignUpForm() {
                     </div>
 
                     <div>
-                        <input type="password" placeholder='Placeholder' className='placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'></input>
+                        <input type="password" placeholder='Placeholder' className='placeholder:text-cool-gray-400 placeholder:text-sm placeholder:leading-5 placeholder:font-normal  rounded border-2 border-gray-500 w-full p-3 mb-2'
+                                onChange={ (event) => { firstName =  event.target.value } }></input>
                 </div>
             </div>
 
             {/** CONSENT TO MARKETING RADIO */}
             <div className=' py-4 items-center p-3 mb-2 space-x-2'>
-                    <input type="checkbox" className=' rounded w-4 h-4 align-middle'></input>
+                    <input type="checkbox" className=' rounded w-4 h-4 align-middle'
+                            onChange={ (event) => { marketingConsent =  event.target.value } }></input>
                     <label className=''> I consent to  receiving marketing material. </label>
             </div>
 
             {/** SIGN UP */}
-            <button className=' border-blue-600 rounded bg-blue-600 w-full h-12 text-white'> Sign Up </button>
+            <button type='button' className=' border-blue-600 rounded bg-blue-600 w-full h-12 text-white'
+                    onClick={ (event) => handleClick(event) }> Sign Up </button>
 
         </form>
 
